cf66941f9d14071da0a5593645eafbd783bda70d
dark32167 <dark32167@yandex.ru>
2017-02-16 01:40:50 +0300

    IsFilePathCorrect done

---
+++ Signature.UnitTests/SignatureMainTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Signature.UnitTests
+{
+    [TestFixture]
+    public class SignatureMainTests
+    {
+        [TestCase("")]
+        [TestCase("1")]
+        [TestCase(@"ControlFilesForTesting\ExistsButEmptyFile.txt")]
+        public void IsFilePathCorrect_InvalidExtensions_ReturnsFalse(string filePath)
+        {
+            //получение пути к контрольным файлам, так как по умолчанию обращение идет в другую дирректорию
+            filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\" + filePath;
+
+            bool result = SignatureMain.IsFilePathCorrect(filePath);            
+
+            Assert.False(result);
+        }
+
+        [TestCase(@"ControlFilesForTesting\CorrectFile.txt")]
+        [TestCase(@"ControlFilesForTesting\correctfile.txt")]
+        [TestCase(@"ControlFilesForTesting\correctfile.TXT")]
+        public void IsFilePathCorrect_ValidExtensions_ReturnsTrue(string filePath)
+        {
+            //получение пути к контрольным файлам, так как по умолчанию обращение идет в другую дирректорию
+            filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\" + filePath;
+
+            bool result = SignatureMain.IsFilePathCorrect(filePath);
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ Signature/SignatureMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signature
+{
+    public class SignatureMain
+    {      
+
+        static void Main(string[] args)
+        {
+            string filePath;
+            do
+            {
+                Console.WriteLine("Введите путь к файлу, сигнатуру которого необходимо получить");
+                filePath = Console.ReadLine();
+            } while (!IsFilePathCorrect(filePath));
+
+            Console.WriteLine("");
+            Console.WriteLine("Нажмите любую клавишу для завершения...");
+            Console.ReadKey(true);
+        }
+
+        public static bool IsFilePathCorrect(String filePath)
+        {
+            if (filePath.Length < 1)
+            {
+                Console.WriteLine("Путь к файлу не может быть пустым, повторите ввод");
+                return false;
+            }
+
+            FileInfo info = new FileInfo(filePath);
+            if (!info.Exists)
+            {
+                Console.WriteLine("Указанный файл не найден, повторите ввод");
+                return false;
+            }
+            if (info.Length == 0)
+            {
+                Console.WriteLine("Указанный файл пуст, повторите ввод");
+                return false;
+            }
+
+            return true;
+
+        }
+    }
+}
